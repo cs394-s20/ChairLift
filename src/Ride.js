fd714021ce8d6fdf3c05fe487652852d7b694440
Margot <margaretsobota@gmail.com>
2020-05-13 11:53:16 -0700

    convert from material ui to react base

--- src/Ride.js
+++ src/Ride.js
@@ -1,44 +1,51 @@
 import React, { Component, useState, useEffect} from 'react';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
+import { StyleSheet, Text, View } from 'react-native';
+import { Container, Header, Content, List, ListItem, Card } from 'native-base';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-      width: '100%'
-    },
-    name: {
-        fontWeight: "bold"
-    }
-}));
+// const useStyles = makeStyles((theme) => ({
+//     root: {
+//       display: 'flex',
+//       width: '100%'
+//     },
+//     name: {
+//         fontWeight: "bold"
+//     }
+// }));
 
 const Ride = (rideObj) => {
-    const classes = useStyles();
     return (
-        <Card className={classes.root}>
+      <Card>
             <List>
-                <ListItem>
-                    <div className = {classes.name}>{rideObj.ride.name}</div> {rideObj.ride.startLoc} (0.5 mi away) --> {rideObj.ride.endLoc}
-                </ListItem>
-                <ListItem>
-                    Departure: {rideObj.ride.departTime}, {rideObj.ride.departDate}
-                </ListItem>
-                <ListItem>
-                    Return: {rideObj.ride.departTime}, {rideObj.ride.departDate}
-                </ListItem>
-                <ListItem>
-                    Contact: {rideObj.ride.phoneNum}
-                </ListItem>
-                <ListItem>
-                    Note: {rideObj.ride.desc}
-                </ListItem>
-            </List>
+              <ListItem>
+                <Text>
+                  {rideObj.ride.name} {rideObj.ride.startLoc} (0.5 mi away) --> {rideObj.ride.endLoc}
+                </Text>
+              </ListItem>
+              <ListItem>
+                <Text>
+                  Departure: {rideObj.ride.departTime}, {rideObj.ride.departDate}
+                </Text>
+              </ListItem>
+              <ListItem>
+                <Text>
+                  Return: {rideObj.ride.departTime}, {rideObj.ride.departDate}
+                </Text>
+              </ListItem>
+              <ListItem>
+                <Text>
+                  Contact: {rideObj.ride.phoneNum}
+                </Text>
+              </ListItem>
+              <ListItem>
+                  <Text> Note: {rideObj.ride.desc} </Text>
+              </ListItem>
+          </List>
             
         </Card> 
+        
     )
 }
+
+
 
 export default Ride;
--- src/RideList.js
+++ src/RideList.js
@@ -2,46 +2,38 @@ import React, { Component, useState, useEffect} from 'react';
 import Ride from './Ride.js';
 import db from './db.js';
 import { StyleSheet, Text, View } from 'react-native';
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List'; 
-import Container from '@material-ui/core/Container'
-import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
+import { Container, Header, Content, List, ListItem, Card } from 'native-base';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-      width: '100%',
-      maxWidth: 360,
-      backgroundColor: theme.palette.background.paper,
-    },
-  }));
+// const useStyles = makeStyles((theme) => ({
+//     root: {
+//       width: '100%',
+//       maxWidth: 360,
+//       backgroundColor: theme.palette.background.paper,
+//     },
+//   }));
 
 const RideList = ({ridesState}) => {
-    const classes = useStyles();
 
     var rides = [];
     if (ridesState.rides) {
         rides = Object.values(ridesState.rides);
     }
 
-
     return (
-        <div className={classes.root}>
-        <Container>
-            <List>
-            {
+      <Container>
+        <Content>
+          <List>
+          {
 
             rides.map(ride => (
-                <div>
-                <ListItem><Ride ride={ride}></Ride></ListItem>
-                <Divider />
-                </div>
-                )
+                <ListItem> 
+                  <Ride ride={ride}></Ride>
+                </ListItem>   )
             ) 
-            }   
-            </List>
-        </Container>
-        </div>
+          }   
+          </List>
+        </Content>
+      </Container>
     )
 
 }
